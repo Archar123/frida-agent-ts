86c099a59552f4a467fc0783f58712812664de94
Archar <111>
2024-10-18 11:55:17 +0800

    更新代码

--- test.ts
+++ test.ts
@@ -13,7 +13,7 @@ function ShowState() {
     }
 }
 
-class ScanGame {
+class AutoMineGame {
     private module_name_winmine = "winmine.exe";
     private module_winmine: Module;
 
@@ -23,7 +23,15 @@ class ScanGame {
     private head: NativePointer = ptr(0);   //棋盘指针
 
     private hWnd: NativePointer = ptr(0);   //窗口句柄
-    private winApi = new WinApi();          //引入的API类
+
+    private start_x = 0;
+    private start_y = 0;
+    private step = 16;
+
+    private MOUSEEVENTF_LEFTDOWN = 0x0002;
+    private MOUSEEVENTF_LEFTUP = 0x0004;
+    private MOUSEEVENTF_RIGHTDOWN = 0x0008;
+    private MOUSEEVENTF_RIGHTUP = 0x0010;
 
     constructor() {
         //获取模块基址
@@ -72,8 +80,274 @@ class ScanGame {
 
     board_repaint() {
         const lpRect = Memory.alloc(4 * 4); //RECT
-        this.winApi.GetClientRect(this.hWnd, lpRect);
-        this.winApi.InvalidateRect(this.hWnd, lpRect, 1);
+        WinApi.GetClientRect(this.hWnd, lpRect);
+        WinApi.InvalidateRect(this.hWnd, lpRect, 1);
+    }
+
+    board_foreground() {
+        let hForeWnd = WinApi.GetForegroundWindow();
+        let dwCurID = WinApi.GetCurrentThreadId();
+        let dwForeID = WinApi.GetWindowThreadProcessId(hForeWnd, ptr(0));
+        WinApi.AttachThreadInput(dwCurID, dwForeID, 1);
+
+        const SW_RESTORE = 9;
+        WinApi.ShowWindow(this.hWnd, SW_RESTORE);
+
+        WinApi.SetForegroundWindow(this.hWnd);
+
+        const HWND_TOPMOST = -1;
+        const HWND_NOTOPMOST = -2;
+        const SWP_NOSIZE = 0x0001;
+        const SWP_NOMOVE = 0x0002;
+        WinApi.SetWindowPos(this.hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+        WinApi.SetWindowPos(this.hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+
+        WinApi.AttachThreadInput(dwCurID, dwForeID, 0);
+    }
+
+    board_location() {
+
+        let lpOrgRect = Memory.alloc(4 * 4);
+        WinApi.GetCursorPos(lpOrgRect);
+
+        let lpRect = Memory.alloc(4 * 4);
+        WinApi.GetWindowRect(this.hWnd, lpRect);
+        console.log("left", lpRect.readU32());
+        console.log("top", lpRect.add(4).readU32());
+
+        this.start_x = lpRect.readU32() + 7;
+        this.start_y = lpRect.add(4).readU32() + 92;
+        let x = 4;
+        let y = 5;
+
+        WinApi.SetCursorPos(this.start_x + this.step * x, this.start_y + this.step * y);
+
+        const MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const MOUSEEVENTF_LEFTUP = 0x0004;
+
+        const MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        const MOUSEEVENTF_RIGHTUP = 0x0010;
+
+        WinApi.MouseEvent(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, WinApi.GetMessageExtraInfo());
+        WinApi.MouseEvent(MOUSEEVENTF_LEFTUP, 0, 0, 0, WinApi.GetMessageExtraInfo());
+    }
+
+    mouse_click(x: number, y: number, left_click: boolean = true) {
+
+        WinApi.SetCursorPos(this.start_x + this.step * x, this.start_y + this.step * y);
+        if (left_click) {
+            WinApi.MouseEvent(this.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, WinApi.GetMessageExtraInfo());
+            WinApi.MouseEvent(this.MOUSEEVENTF_LEFTUP, 0, 0, 0, WinApi.GetMessageExtraInfo());
+        }
+        else {
+            WinApi.MouseEvent(this.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, WinApi.GetMessageExtraInfo());
+            WinApi.MouseEvent(this.MOUSEEVENTF_RIGHTUP, 0, 0, 0, WinApi.GetMessageExtraInfo());
+        }
+    }
+
+    board_click() {
+
+        //记录鼠标位置
+        let lpOrgRect = Memory.alloc(4 * 4);
+        WinApi.GetCursorPos(lpOrgRect);
+
+        //加载棋盘数据
+        this.load_board_info();
+
+        //获取棋盘位置
+        let lpRect = Memory.alloc(4 * 4);
+        WinApi.GetWindowRect(this.hWnd, lpRect);
+        this.start_x = lpRect.readU32() + 7;
+        this.start_y = lpRect.add(4).readU32() + 92;
+
+        //遍历棋盘，按行遍历
+        for (let i = 1; i < this.height + 2; i++) {
+            //按列遍历
+            for (let j = 1; j < this.width + 2; j++) {
+                let byte_data = this.head.add(j + 0x20 * i).readU8();
+                //标记地雷
+                if (byte_data == 0x8F) {
+                    this.mouse_click(j, i, false);
+                    continue;
+                }
+                //点击无雷区
+                if (byte_data == 0x0F) {
+                    this.mouse_click(j, i);
+                    continue;
+                }
+            }
+        }
+
+        //鼠标归位
+        WinApi.SetCursorPos(lpOrgRect.readU32(), lpOrgRect.add(4).readU32());
+    }
+}
+
+class HookMineGame {
+    private module_name_winmine = "winmine.exe";
+    private module_winmine: Module;
+
+    constructor() {
+        //获取模块基址
+        this.module_winmine = Process.getModuleByName(this.module_name_winmine);
+    }
+
+    memory_utils() {
+        
+        //转成二进制
+        let p = ptr(0x00210604);
+        let pattern = p.toMatchPattern();
+        console.log("pattern", pattern);
+
+        //函数头特征
+        pattern = "55 8b ec";
+
+        Memory.scan(this.module_winmine.base, this.module_winmine.size, pattern, {
+            onMatch: (address, size) => {
+                //console.log("onMatch", size, address, address.sub(this.module_winmine.base));
+            },
+            onError: (reason) => {
+                console.log(reason);
+            },
+            onComplete: () => {
+                console.log("Scan Complete!");
+            }
+        });
+
+        let matches = Memory.scanSync(this.module_winmine.base, this.module_winmine.size, pattern);
+        for (const iterator of matches) {
+            console.log(JSON.stringify(iterator));
+        }
+
+        /*
+        let m1 = Memory.alloc(Process.pageSize);
+        console.log("protect", JSON.stringify(Process.getRangeByAddress(m1)));
+        Memory.protect(m1, Process.pageSize, "r-x");
+        console.log("protect", JSON.stringify(Process.getRangeByAddress(m1)));*/
+
+        //shellcode调用API
+        let lpText = Memory.allocUtf16String("This is a string！");
+        let lpCaption = Memory.allocUtf16String("Caption");
+
+        let m2 = Memory.alloc(Process.pageSize);
+        console.log("m2 addr=", m2);
+        let address = Module.getExportByName("User32.dll", "MessageBoxW");
+
+        Memory.patchCode(m2, Process.pageSize, (code) => {
+            let asm = new X86Writer(code);
+            asm.putPushU32(0x00000001);
+            asm.putPushU32(lpCaption.toUInt32());
+            asm.putPushU32(lpText.toUInt32());
+            asm.putPushU32(0);
+
+            asm.putCallAddress(address);
+            asm.putRet();
+
+            asm.flush();
+        });
+
+        this.show_asm(m2);
+        
+        // call 
+        //let func = new NativeFunction(m2, "void", []);
+        //func();
+    }
+
+    show_asm(start: NativePointer, length: number = 10) {
+
+        for (let index = 0; index < length; index++) {
+            let inst = Instruction.parse(start);
+            // console.log(JSON.stringify(inst));
+            let byteArray = start.readByteArray(inst.size);
+            let byteCode = Array.prototype.slice.call(new Uint8Array(byteArray!));
+            let mCode = byteCode.map(x => x.toString(16).padStart(2, "0")).join(" ").toUpperCase();
+            console.log(inst.address.toString().toUpperCase().replace("0X", "0x"), mCode.padEnd(14, " "), "\t", inst.toString().toUpperCase().replace("0X", "0x"));
+
+            start = inst.next;
+            if (start.readU32() == 0) break;
+        }
+    }
+
+    hook_func() {
+        //DispatchMessageW
+        let address = Module.getExportByName("User32.dll", "DispatchMessageW");
+
+        console.log("Main Thread:", Process.enumerateThreads()[0].id);
+        console.log("CurrentThreadId", Process.getCurrentThreadId());
+
+        let listener = Interceptor.attach(address,{
+            onEnter(this, args) {
+                //console.log("onEnter context", JSON.stringify(this.context));
+
+                // typedef struct tagMSG {
+                //   HWND   hwnd;
+                //   UINT   message;
+                //   WPARAM wParam;
+                //   LPARAM lParam;
+                //   DWORD  time;
+                //   POINT  pt;
+                //   DWORD  lPrivate;
+                // } MSG, *PMSG, *NPMSG, *LPMSG;                
+                console.log("onEnter tid=", Process.getCurrentThreadId());
+
+                let msg = args[0];
+                console.log("hwnd", msg.readPointer());
+                console.log("message", msg.add(4).readPointer());
+                console.log("wParam", msg.add(8).readPointer());
+                console.log("lParam", msg.add(12).readPointer());
+                console.log("pt", msg.add(20).readPointer());
+                console.log("lPrivate", msg.add(24).readPointer());
+            },
+            onLeave(this, retval) {
+                //console.log("onLeave context", JSON.stringify(this.context));
+                console.log("ret=",retval);
+                //加上这个，相当于只Hook一次
+                //listener.detach();
+            },
+        })
+    }
+
+    module_utils() {
+
+        let moduleMap = new ModuleMap((m: Module) => { return m.name.endsWith("dll"); });
+        for (const iterator of moduleMap.values()) {
+            console.log(JSON.stringify(iterator));
+        }
+    }
+
+    //很容易崩
+    access_mon() {
+        /*
+        let rangs = Process.enumerateMallocRanges().filter(x => x.size > 2000);
+        for (const iterator of rangs) {
+            console.log(JSON.stringify(iterator));
+        }*/
+        /*
+        MemoryAccessMonitor.enable(
+            {   
+                base : this.module_winmine.base,
+                size : 0x1000
+            },
+            {
+                onAccess(details) {
+                    console.log("address", details.address, "from", details.from, "operation", details.operation, "pageIndex", details.pageIndex, "pagesCompleted", details.pagesCompleted, "pagesTotal", details.pagesTotal, "rangeIndex", details.rangeIndex);
+                    console.log("===");
+                },
+            });
+        console.log("MemoryAccessMonitor OK");*/
+
+        let pattern = "4d 5a";
+        Memory.scan(this.module_winmine.base, 0x1000, pattern, {
+            onMatch: (address, size) => {
+                console.log("onMatch", size, address, address.sub(this.module_winmine.base));
+            },
+            onError: (reason) => {
+                console.log(reason);
+            },
+            onComplete: () => {
+                console.log("Scan Complete!");
+            }
+        });
     }
 }
 
@@ -81,15 +355,28 @@ function main()
 {
     ///DevTools里在这里下断点。然后主动调用main()即可断下
     //一旦import了多个模块之后，就无法直接试用main()执行了，会提示函数无法找到，此时需要用rpc.exports.main()
-    console.log("Start");
     ShowState();
+/*
+    console.log("AutoMineGame Start");
+    let game = new AutoMineGame();
+    game.visit_board(true);
+    game.board_info();
+    game.board_repaint();
+    game.board_foreground();
+    game.board_location();
+    game.board_click();
+    console.log("AutoMineGame End");
+    */
 
-    let scanner = new ScanGame();
-    scanner.visit_board(true);
-    scanner.board_info();
-    scanner.board_repaint();
+    console.log("HookMineGame Start");
 
-    console.log("End");
+    let hook = new HookMineGame();
+    //hook.memory_utils();
+    //hook.hook_func();
+    //hook.module_utils();
+    hook.access_mon();
+
+    console.log("HookMineGame End");
 }
 
 //导出一个函数，方便我们主动调用，进行调试。
@@ -98,6 +385,7 @@ rpc.exports = {
         main();
     },
   };
+
 
 //
 main()
